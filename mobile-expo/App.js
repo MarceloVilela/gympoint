ee297be5e560ad6ea53b8d8c736209916463aaaa
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2020-01-01 21:58:27 -0300

    Mobile utilizando expo

---
+++ mobile-expo/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import App from "./src";
+
+console.disableYellowBox = true;
+
+export default function Index() {
+  return <App />;
+}
+
+/*
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>333Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+*/
