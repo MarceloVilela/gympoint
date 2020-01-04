95787bb98b8dc5d0a1a7ab4a1342ff8dcb9283f9
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2020-01-04 10:56:05 -0300

    [Frontend] Redux - Correcao do loading em caso de erro na autenticacao

--- frontend/src/store/modules/auth/actions.js
+++ frontend/src/store/modules/auth/actions.js
@@ -12,16 +12,9 @@ export function signInSuccess(token, user) {
   };
 }
 
-export function signUpRequest(name, email, password) {
-  return {
-    type: '@auth/SIGN_UP_REQUEST',
-    payload: { name, email, password },
-  };
-}
-
 export function signFailure() {
   return {
-    type: '@auth/SIGN_FAILURE',
+    type: '@auth/SIGN_IN_FAILURE',
   };
 }
 
