a1c50b7ee34173ba2985590a1bf2bac8f4ebc384
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2020-01-05 11:25:45 -0300

    [mobile] remove projeto iniciado pelo react-native-cli

--- mobile/src/pages/Help/Index/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-export const List = styled.FlatList.attrs({
-  showsVerticalScrollIndicator: false,
-  // contentContainerStyle: { padding: 30 },
-})`
-  margin-top: 20px;
-`;
